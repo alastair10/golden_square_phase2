59f3604dab4edd7f148f83325dcdb9e3ff9d7256
Alastair <alastair.chau@gmail.com>
2022-10-12 10:45:40 +0100

    added more tests and refactored

--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,8 +1,24 @@
 require 'make_snippet'
 
 RSpec.describe "check snippet method" do
-  it "returns a list of words" do
-    result = snippet("hello, world, goodbye, coding, ruby, extra")
-    expect(result).to eq "hello, world, goodbye, coding, ruby,..."
+  context "when given a long string" do
+    it "returns only the first 5 words with a ..." do
+      result = snippet("hello world goodbye world coding ruby extra")
+      expect(result).to eq "hello world goodbye world coding..."
+    end
   end
+
+  context "given an empty string" do
+    it "returns the empty string" do
+      result = snippet("")
+      expect(result).to eq ""
+    end
+  end
+
+  context "given a 5 word string" do
+    it "returns the 5 word string" do
+      result = snippet("hello world goodbye world coding")
+      expect(result).to eq "hello world goodbye world coding"
+    end
+  end  
 end
