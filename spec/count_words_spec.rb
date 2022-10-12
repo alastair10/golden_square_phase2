35d7ef7d8b968991998c0bc9b2bbaf5299172e77
Alastair <alastair.chau@gmail.com>
2022-10-12 11:07:15 +0100

    added extra test

--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe "method takes string and returns the number of words in that stri
       expect(result).to eq 3
     end
   end
+
+  context "given empty str" do
+    it "returns 0" do
+      result = counter("")
+      expect(result).to eq 0
+    end
+  end
+
 end
